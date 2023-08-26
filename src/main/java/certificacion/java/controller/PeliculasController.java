e98ec3d7bfc2d89114f1643d62c2ccc157d5668d
Diego <dapm@jupiter.local>
2023-08-26 13:18:53 -0400

    agregar controller para listar peliculas

---
+++ src/main/java/certificacion/java/controller/PeliculasController.java
@@ -0,0 +1,32 @@
+package certificacion.java.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import certificacion.java.dao.PeliculasDAO;
+import certificacion.java.model.Peliculas;
+
+@Controller
+@RequestMapping("/")
+public class PeliculasController {
+
+	private final PeliculasDAO peliculasDAO;
+
+	@Autowired
+	public PeliculasController(PeliculasDAO peliculasDAO) {
+		this.peliculasDAO = peliculasDAO;
+	}
+	
+    @GetMapping("/listadoPeliculas")	
+    public String listadoPeliculas(Model model) {
+      List<Peliculas> listaPeliculas = peliculasDAO.findAll();
+      model.addAttribute("listaPeliculas", listaPeliculas);
+      return "listadopeliculas";
+    }
+
+}
