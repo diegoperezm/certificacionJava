ebf4943edbf8dd6908f18ae7e3d1176c725a12c4
diegoperezm <diegoperezcorreo@gmail.com>
2023-09-05 19:14:35 -0300

    Agregar findPeliculasById

--- src/main/java/certificacion/java/controller/PeliculasController.java
+++ src/main/java/certificacion/java/controller/PeliculasController.java
@@ -2,11 +2,17 @@ package certificacion.java.controller;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import certificacion.java.model.Peliculas;
 import certificacion.java.services.PeliculasService;
 import certificacion.java.services.dto.PeliculasDTO;
 
@@ -14,6 +20,7 @@ import certificacion.java.services.dto.PeliculasDTO;
 @RequestMapping("/")
 public class PeliculasController {
 
+    private final Logger logger = LoggerFactory.getLogger(PeliculasController.class);
 	private final PeliculasService peliculasService;
 
 	public PeliculasController(PeliculasService peliculasService) {
@@ -27,4 +34,19 @@ public class PeliculasController {
 		return "listadopeliculas";
 	}
 
+	@GetMapping("/calificarPelicula/{id}")
+	public String calificarPelicula(@PathVariable Long id,Model model) {
+		Peliculas pelicula = peliculasService.findPeliculaById(id);
+        model.addAttribute("pelicula", pelicula);
+	    return "calificar";	
+	}
+ 
+    			
+	@PostMapping("/agregarCalificacion/{id}")
+	public String calificarPelicula(@PathVariable Long id,
+			                        @RequestParam("calificacion") int calificacion , Model model) {
+	Peliculas pelicula = peliculasService.findPeliculaById(id);
+	 logger.info("calificación {}  ID: {}", calificacion, pelicula.getId_pelicula());
+	return "redirect:/listadoPeliculas";
+	}
 }
--- src/main/java/certificacion/java/services/PeliculasService.java
+++ src/main/java/certificacion/java/services/PeliculasService.java
@@ -20,41 +20,31 @@ public class PeliculasService {
 	@Autowired
 	PeliculasDAO peliculasDAO;
 
-	@Autowired
-	CalificacionesDAO calificacionesDAO;
-
-	public PeliculasService(PeliculasDAO peliculasDAO, CalificacionesDAO calificacionesDAO) {
-		super();
+	public PeliculasService(PeliculasDAO peliculasDAO) {	
 		this.peliculasDAO = peliculasDAO;
-		this.calificacionesDAO = calificacionesDAO;
 	}
 
 	public List<PeliculasDTO> listarPeliculas() {
 		List<PeliculasDTO> peliculasDTO = new ArrayList<PeliculasDTO>();
 		List<Peliculas> peliculas = peliculasDAO.findAll();
-		
-	   for(Peliculas pelicula : peliculas) {
-  		   List<Integer> puntuaciones = getPuntaciones(pelicula);
-		   peliculasDTO.add(new PeliculasDTO(
-                              pelicula.getId_pelicula(),
-                              puntuaciones,
-                              pelicula.getAnio_lanzamiento(),
-                              pelicula.getTitulo(),
-                              pelicula.getGenero().getNombre_genero()
-		      		   ));
-	   }
+
+		for (Peliculas pelicula : peliculas) {
+			List<Integer> puntuaciones = getPuntaciones(pelicula);
+			peliculasDTO.add(new PeliculasDTO(pelicula.getId_pelicula(), puntuaciones, pelicula.getAnio_lanzamiento(),
+					pelicula.getTitulo(), pelicula.getGenero().getNombre_genero()));
+		}
 		return peliculasDTO;
 	}
-	
-	private List<Integer> getPuntaciones(Peliculas pelicula) {
-	   List<Integer> puntuaciones = pelicula
-			                     .getCalificaciones()
-			                     .stream()
-			                     .map(Calificaciones::getPuntuacion)
-			                     .collect(Collectors.toList());
-	   return puntuaciones;
+
+	public Peliculas findPeliculaById(Long id) {
+		Optional<Peliculas> optPelicula = peliculasDAO.findById(id);
+		return optPelicula.orElseThrow(() -> new RuntimeException("Pelicula no encontrada,ID: " + id));
 	}
 
-
+	private List<Integer> getPuntaciones(Peliculas pelicula) {
+		List<Integer> puntuaciones = pelicula.getCalificaciones().stream().map(Calificaciones::getPuntuacion)
+				.collect(Collectors.toList());
+		return puntuaciones;
+	}
 
 }
